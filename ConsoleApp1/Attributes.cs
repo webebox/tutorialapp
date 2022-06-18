2bc4f39d088f897bd00579a3afd7de2d3252d682
Ali <Ali@DESKTOP-594DIFR>
2022-06-18 11:17:44 +0430

    2

---
+++ ConsoleApp1/Attributes.cs
@@ -0,0 +1,63 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Attributes
+    {
+
+        // Conditional
+        [Conditional("DEBUG")]  // this method runs only in Debug mode
+        public void DebugMethod()
+        {
+            Console.WriteLine("Debug Method");
+        }
+
+        // Obsolete
+        [Obsolete("Don't use OldMethod, use NewMethod instead", true)]
+        public string OldMethod()
+        {
+            return "old";
+        }
+
+        // Custom Attribute Valuing
+        [CutomAttribute(56, "Zara Ali", "19/10/2012")]
+        public void CustomMethod()
+        {
+
+        }
+
+    }
+
+    // Custom Attribute Declaration
+    [AttributeUsage(
+    AttributeTargets.Class |
+    AttributeTargets.Constructor |
+    AttributeTargets.Field |
+    AttributeTargets.Method |
+    AttributeTargets.Property,
+    AllowMultiple = true)]
+    public class CutomAttribute : System.Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+
+        public CutomAttribute(int bg, string dev, string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+        public int BugNo => bugNo;
+        public string Developer => developer;
+        public string LastReview => lastReview;
+    }
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Reflection;
 
-namespace ConsoleApp1 
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var a = new Attributes();
+
+            // Conditional
+            a.DebugMethod();
+
+            // Obsolete            
+            // Console.WriteLine(a.OldMethod());
+
+            // Custom Attribute
+            MethodBase method = typeof(Attributes).GetMethod("CustomMethod");
+            CutomAttribute attr = (CutomAttribute)method.GetCustomAttributes(typeof(CutomAttribute), true)[0];
+            Console.WriteLine("Developer is : " + attr.Developer);
+
         }
     }
 }
