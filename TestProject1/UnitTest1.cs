4a8fde48d046edf5a2345dcdfc5d4a0f50acd043
Ali <Ali@DESKTOP-594DIFR>
2022-06-20 10:33:07 +0430

    8

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+
+
+
+            // act
+            double actual = a + b;
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
